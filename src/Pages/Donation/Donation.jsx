739272d4220fe516446614c6bd852094dabb3ab3
Imtiaz <im3azchowdhury@gmail.com>
2023-09-26 12:22:19 +0600

    donation page see all button created and feature added

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -4,6 +4,7 @@ import DonationPageCard from "../../Components/Donations/DonationPageCard";
 const Donation = () => {
   const [donated, setDonated] = useState([]);
   const [noData, setNoData] = useState(false);
+  const [isShow, setIsShow] = useState(false);
 
   useEffect(() => {
     const donatedItems = JSON.parse(localStorage.getItem("donated"));
@@ -16,6 +17,7 @@ const Donation = () => {
   }, []);
 
   // console.log(donated);
+  // console.log(isShow);
 
   return (
     <div>
@@ -25,10 +27,31 @@ const Donation = () => {
         </p>
       ) : (
         <div>
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mx-0 md:mx-16 lg:mx-0 " >
-            {donated.map((item) => (
-              <DonationPageCard key={item.id} donated={item}></DonationPageCard>
-            ))}
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mx-0 md:mx-16 lg:mx-0 ">
+            {isShow
+              ? donated.map((item) => (
+                  <DonationPageCard
+                    key={item.id}
+                    donated={item}
+                  ></DonationPageCard>
+                ))
+              : donated
+                  .slice(0, 4)
+                  .map((item) => (
+                    <DonationPageCard
+                      key={item.id}
+                      donated={item}
+                    ></DonationPageCard>
+                  ))}
+          </div>
+
+          <div className={isShow ? "hidden" : "block"}>
+            <button
+              onClick={() => setIsShow(!isShow)}
+              className="btn btn-primary block my-10 mx-auto"
+            >
+              See all
+            </button>
           </div>
         </div>
       )}
